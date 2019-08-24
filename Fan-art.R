446298085427761b56f090b686a614da92d91a29
joemarlo <joe.marlo.09@gmail.com>
2019-08-24 10:50:42 -0400

    Added different designs to plot

--- Fan-art.R
+++ Fan-art.R
@@ -8,7 +8,8 @@ library(png)
 buildFan <- function(hue = 0.5, saturation = 0.20, value = 0.55, alpha = 0.5,
                      hue.range = 0, saturation.range = 0, value.range = 0, alpha.range = 0,
                      x.end = NULL, y.end = NULL, x.center = NULL, y.center = NULL,
-                     random.center = FALSE, segment.width = 8, n.segments = 200, depth = FALSE){
+                     random.center = FALSE, segment.width = 8, n.segments = 200,
+                     depth = FALSE, rescale = FALSE){
   
   #function builds a single fan plot
   #random vectors are generated if no vectors are provided
@@ -16,9 +17,7 @@ buildFan <- function(hue = 0.5, saturation = 0.20, value = 0.55, alpha = 0.5,
   
   # set the center points of the segments
   x.center <- if (!is.null(x.center)) x.center else if(random.center == FALSE) 0 else if(is.null(x.end)) runif(1, -1, 1) else runif(1, min(x.end), max(x.end))
-  x.center <- if (!is.null(x.center)) x.center else rep(x.center, n.segments)
   y.center <- if (!is.null(y.center)) y.center else if(random.center == FALSE) 0 else if(is.null(y.end)) runif(1, -1, 1) else runif(1, min(y.end), max(y.end))
-  y.center <- if (!is.null(y.center)) y.center else rep(y.center, n.segments)
   
   # set the end of the segments. If vectors are provided then add some randomness
   x.end <- if(is.null(x.end)) rnorm(n.segments, 0, 1) else x.end * runif(n.segments, 0.3)
@@ -49,28 +48,28 @@ buildFan <- function(hue = 0.5, saturation = 0.20, value = 0.55, alpha = 0.5,
     y.end <- y.end[vector.order]
     
     # sort the colors by strength
-    # h <- sort(h) # leave out hue so there is still some color variance per plot
-    s <- sort(s)
-    v <- sort(v)
+    # leave out hsv o there is still some color variance per plot
+    # h <- sort(h)
+    # s <- sort(s)
+    # v <- sort(v)
     alpha <- sort(alpha)
     lwd <- sort(lwd)
   }
   
   #rescale the x, y vectors so they're between -1 and 1
-  rescale <- function(x){(x - min(x))/(max(x) - min(x)) * 2 - 1}
-  x.end <- rescale(x.end)
-  y.end <- rescale(y.end)
-  
-  # set the base vector for determining the plot size
-  x <- if(is.null(x.end)) seq(-1, 1, length = n.segments) else x.end
-  y <- if(is.null(y.end)) seq(-1, 1, length = n.segments) else y.end
+  if (rescale){
+    rescale.vector <- function(x){(x - min(x))/(max(x) - min(x)) * 2 - 1}
+    x.end <- rescale.vector(x.end)
+    y.end <- rescale.vector(y.end)
+  }
   
   #set graphical parameters
   par(mar = rep(1, 4),
       bty = "n")
   
   # draw the plot template
-  plot(x, y,
+  plot(x = 0,
+       y = 0,
        xlim = c(-1, 1),
        ylim = c(-1, 1),
        type = 'n',
@@ -94,13 +93,12 @@ buildFan <- function(hue = 0.5, saturation = 0.20, value = 0.55, alpha = 0.5,
 # function wraps around buildFan to automate the printing of multiple plots ----------------------------------------------------
 
 printFans <- function(hsv.array = seq(0, 1, length.out = 100), filename = "fans.png",
-                      output.image.width = 4608, output.image.height = 2880, ...){
+                      output.image.width = 4608, output.image.height = 2880, background = "seashell", ...){
   
   # function automates the printing and saving of buildFan
   # hsv.array is the array colors to map the buildFan function over. If only one vector is provided then it is
   # assumed to be hue. If two, then hue and saturation. If three, hue then saturation then value
   # length / nrow of hsv.array determines how many plots there will be
-  # image is a logical. If true then hsv.array should be an image that is then converted to an hsv.array
   # n.segments is number of segments per plot
   
   # n.plots is the number of plots to be printed determined by the length/nrow of hsv.array
@@ -114,7 +112,7 @@ printFans <- function(hsv.array = seq(0, 1, length.out = 100), filename = "fans.
       height = output.image.height)
     
   # set graphical parameters
-  par(bg = "grey15",
+  par(bg = background,
       mar = rep(2, 4),
       mfrow = c(sqrt(n.plots), sqrt(n.plots)),
       bty = "n")
@@ -138,3 +136,68 @@ buildFan()
 
 # build a panel of fans and save it
 printFans()
+
+
+# build different designs -------------------------------------------------
+
+#random
+buildFan(n.segments = 1000,
+         x.end = runif(1000, -1, 1),
+         y.end = runif(1000, -1, 1),
+         x.center = rep(0, 1000),
+         y.center = rep(0, 1000))
+
+# define the colors to map the function over
+n.plots <- 9^2
+hues <- tibble(hue = seq(0.35, 0.55, length = n.plots),
+               saturation = seq(0.25, 0.1, length = n.plots))
+
+# specific colors with random segments
+printFans(hsv.array = hues,
+          n.segments = 250,
+          depth = TRUE,
+          hue.range = 0.5,
+          saturation.range = 0.10,
+          value = 0.45,
+          value.range = 0.20,
+          alpha = 0.3,
+          alpha.range = 0.30,
+          segment.width = 10,
+          random.center = TRUE)
+
+# semi circle
+n.segments <- 150
+base.vector <- seq(0, pi, length = n.segments)
+x.vector <- cos(base.vector)
+y.vector <- sin(base.vector) * 2 - 1
+printFans(hsv.array = hues,
+          n.segments = n.segments,
+          x.end = x.vector,
+          y.end = y.vector,
+          x.center = 0,
+          y.center = -1,
+          hue.range = 0.20,
+          saturation.range = 0.20,
+          value.range = 0.20,
+          alpha.range = 0.40,
+          alpha = 0.3,
+          segment.width = 20)
+
+# circle
+n.segments <- 200
+base.vector <- seq(0, pi*2, length = n.segments)
+x.vector <- cos(base.vector) * 2
+y.vector <- sin(base.vector) * 2
+printFans(hsv.array = hues,
+          n.segments = n.segments,
+          x.end = x.vector,
+          y.end = y.vector,
+          hue.range = 0.30,
+          saturation.range = 0.20,
+          value.range = 0.40,
+          alpha.range = 0.40,
+          alpha = 0.3,
+          output.image.width = 2500,
+          output.image.height = 2500,
+          depth = TRUE,
+          background = "seashell2")
